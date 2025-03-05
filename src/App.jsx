07d8892b78b7d639a2fffce84f7877972b2645de
MargoTech <margobartashevich@gmail.com>
2025-03-05 22:52:38 +0200

    Adding css

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from "react";
-import Header from "./Header";
+import { useState } from "react";
 import ExpenseForm from "./components/ExpenseForm";
+import "./App.css";
 
 const App = () => {
-  const [expenses, setExpenses] = useState([]);
+  const [expenses, setExpenses] = useState([
+    { title: "Coffee", amount: 5 },
+    { title: "Taxi", amount: 7 },
+  ]);
 
   const addExpense = (expense) => {
     setExpenses([...expenses, expense]);
   };
 
   return (
-    <div>
-      <Header
-        title="Expense Tracker"
-        description="Track your expenses easily"
-      />
-      <main>
-        <h2>Your Expenses</h2>
-      </main>
+    <div className="container">
+      <h1>Tracker expenses</h1>
+      <ExpenseForm onAddExpense={addExpense} />
+
+      <ul>
+        {expenses.map((expense, index) => (
+          <li key={index}>
+            <span>{expense.title}</span>
+            <span>{expense.amount.toLocaleString()}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
