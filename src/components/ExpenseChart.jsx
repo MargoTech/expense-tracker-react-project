c6063a30de8c2ddf55380c964aa9ad9ddd2f6c4a
MargoTech <margobartashevich@gmail.com>
2025-10-17 18:03:10 +0200

    Working with ExpenseChart

--- src/components/ExpenseChart.jsx
+++ src/components/ExpenseChart.jsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 import { useExpenses } from "../context/ExpenseContext";
+import { div } from "framer-motion/client";
 
 const COLORS = ["#FF6384", "#36A2EB", "#4CAF50", "#9E9E9E"];
 
@@ -17,5 +25,30 @@ const ExpenseChart = () => {
     }
   );
 
-  return;
+  return (
+    <div className="bg-white dark:bg-gray-800 shadow-md rounded-xl p-4 mt-6">
+      <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-white">
+        Expenses by Category
+      </h2>
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={100}
+            label
+          >
+            {data.map((entry, index) => (
+              <Cell key={index} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
